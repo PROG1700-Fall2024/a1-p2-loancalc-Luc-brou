9cb2e41f66b41c538384534f3b850e0562654075
Luc-brou <luc.r.brousseau@gmail.com>
2024-12-03 10:37:01 -0400

    Final version

--- A1-P2-LoanCalc.py
+++ A1-P2-LoanCalc.py
@@ -4,11 +4,26 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
-
-
-
-
-
+    # when user inputs x dollars, calculate interest rate and give weekly payment.
+    # should have prompts for dollars, interest, and years
+    # user inputs dollar amount
+    # user inputs interest rate
+    # user inputs number of years
+    
+    print("Hello! This program is made to calculate weekly loan payments.")
+    print("")
+    loan=input("Please input the loan amount in dollars.")
+    loanAmount=float(loan)
+    print("")
+    interest=input("Please enter the interest rate. (%)")
+    interestRate=float(interest)
+    print("")
+    years=input("Please enter the amount of years the loan is for.")
+    numberYears=float(years)
+    print("")
+    weeklyPayment=print("")
+    print("")
+    print("You will have to pay",weeklyPayment,"per week.")
 
 
     # YOUR CODE ENDS HERE
